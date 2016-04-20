e81353640a406fe4f41bb9e4935307183fda98fe
reco <reco@liao.com>
2016-04-20 15:50:35 +0800

    add the logic to check arguments whether are valid

--- src/inherits.js
+++ src/inherits.js
@@ -1,4 +1,13 @@
 module.exports = (subClass, superClass) => {
+	if (subClass === undefined || subClass === null)
+		throw new TypeError(`The sub class to 'inherits' must not be null or undefined.`);
+
+	if (superClass === undefined || superClass === null)
+		throw new TypeError(`The super class to 'inherits' must not be null or undefined.`);
+
+	if (superClass.prototype === undefined)
+		throw new TypeError(`The super class to 'inherits' must have a prototype.`);
+
 	Reflect.setPrototypeOf(subClass.prototype, superClass.prototype);
 	Reflect.defineProperty(subClass.prototype, 'constructor', {
 		value: subClass,
