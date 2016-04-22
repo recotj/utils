7f8eb4628c7d42a1e0bafa5ad121c779003e05f2
reco <reco@liao.com>
2016-04-22 17:49:31 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -139,7 +139,6 @@ gulp.task('build:min', ['entry'], () => {
 
 gulp.task('install-deps', (done) => {
 	const pkgJSON = require('./package.json');
-	console.log('package.json: ', JSON.stringify(pkgJSON));
 	pkgJSON._where = pkgJSON._where || process.cwd();
 
 	require('check-deps')(pkgJSON, done);
